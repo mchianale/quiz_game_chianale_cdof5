220dc97f9f0875cedb0ec98fc78c92a6251c4362
Matteo Chianale <matteo.chianale75@gmail.com>
2024-01-29 18:33:35 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 29 18:31:30 2024
+
+@author: Matteo
+"""
+
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='quiz_game_chianale_cdof5',
+ version='1.0',
+ author='mchianale',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
