0d00297d6f5c40939557809786727e356a1e7220
Matteo Chianale <matteo.chianale75@gmail.com>
2024-01-30 17:35:07 +0100

    system to auto update read me

--- bank/create_bank_questions.py
+++ bank/create_bank_questions.py
@@ -36,3 +36,15 @@ print(questions)
 file = open('all_questions.json', 'w')
 json.dump(questions, file, indent=4)
 file.close()
+
+#update Read me
+file = open('../README.md', 'r')
+lines = file.readlines()
+arr = lines[4].split('**')
+arr[1] = len(questions)
+lines[4] = arr[0] + '**' + str(arr[1]) + '**' + arr[2]
+file.close()
+
+file = open('../README.md', 'w')
+file.writelines(lines)
+file.close()
---
+++ update_README.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 30 17:25:50 2024
+
+@author: Matteo
+"""
+
+import json
+file = open('bank/all_questions.json', 'r')
+questions = json.load(file)
