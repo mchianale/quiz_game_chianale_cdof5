348c1993babc07e7de052efd466558e2b8fd0852
Matteo Chianale <matteo.chianale75@gmail.com>
2024-01-30 17:03:27 +0100

    increase bank of questions & update random

---
+++ bank/create_bank_questions.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 30 16:27:27 2024
+
+@author: Matteo
+"""
+
+import json
+ 
+
+#get first questions 
+file = open('questions0.json', 'r')
+questions = json.load(file)
+file.close()
+
+
+#get second bank of questions
+file = open('questions1.json', 'r')
+questions1 = json.load(file)
+equivalent_answer = {'A' : 1, 'B' : 2, 'C' : 3, 'D' : 4}
+ 
+for question in questions1:
+    q = {}
+    new_question = {
+        'question' : question['question'],
+        'options' : [question['A'],question['B'],question['C'],question['D']],
+        'answer' : equivalent_answer[question['answer']],
+        'category': ''
+    }
+    questions.append(new_question)
+file.close()
+
+
+#save all
+print(questions)
+file = open('all_questions.json', 'w')
+json.dump(questions, file, indent=4)
+file.close()
--- main.py
+++ main.py
@@ -4,96 +4,33 @@ Created on Mon Jan 29 18:10:23 2024
 
 @author: Matteo
 """
-<<<<<<< HEAD
-import json
-file = open('questions.json', 'r')
-questions = json.load(file)
-score = 0
  
-=======
+import json
 import random
 
+#quetsions informations
+file = open('bank/all_questions.json', 'r')
+questions = json.load(file)
+TOTAL_QUESTIONS = len(questions)
+id_questions = [i for i in range(0, TOTAL_QUESTIONS)]
+selected_questions = random.sample(id_questions, 10)
+print(selected_questions)
+
+#user 
 score = 0
-questions  = [
-    {
-        "question": "What is the largest planet in our solar system?",
-        "options": ["Jupiter", "Saturn", "Neptune"],
-        "answer": 1
-    },
-    {
-        "question": "What galaxy is Earth located in?",
-        "options": ["Andromeda Galaxy", "Whirlpool Galaxy", "Milky Way Galaxy"],
-        "answer": 3
-    },
-    {
-        "question": "What is the name of the red planet?",
-        "options": ["Mars", "Venus", "Mercury"],
-        "answer": 1
-    },
-    {
-        "question": "How many moons does Earth have?",
-        "options": ["1", "2", "3"],
-        "answer": 1
-    },
-    {
-        "question": "What is the hottest planet in our solar system?",
-        "options": ["Mercury", "Venus", "Mars"],
-        "answer": 2
-    },
-    {
-        "question": "What planet is known as the 'Ringed Planet'?",
-        "options": ["Jupiter", "Saturn", "Uranus"],
-        "answer": 2
-    },
-    {
-        "question": "Which planet is known for its Great Red Spot?",
-        "options": ["Jupiter", "Mars", "Neptune"],
-        "answer": 1
-    },
-    {
-        "question": "Which is the closest star to Earth?",
-        "options": ["Proxima Centauri", "Sirius", "The Sun"],
-        "answer": 3
-    },
-    {
-        "question": "What is the term for a year on Mars?",
-        "options": ["Solar Year", "Martian Year", "Red Year"],
-        "answer": 2
-    },
-    {
-        "question": "Which of these is not a dwarf planet?",
-        "options": ["Pluto", "Eris", "Neptune"],
-        "answer": 3
-    }
-]
-score =0
-for i in range(15):
-    l=random.randint(0,9)
-    print('Question {}, {}'.format(i+1,questions[l]['question']))
-    for j,o in enumerate(questions[l]['options']):
-        print('{} - {}'.format(j+1, o))
-    a = input('Please select the rigth answer : ')
-    if a == str(questions[l]['answer']):
-        score += 1
-        print('Good ! current score : {}/{}\n'.format(score, 15))
-    else:
-        print('False ! current score : {}/{}\n'.format(score, 15))
->>>>>>> 98c13037d2009817ad5e5f919b5db72b9b416eb2
-
-print('Final score : {}/{}'.format(score, 15))
-
-
-'''
-for i,q in enumerate(questions):
-    print('Question {}, {}'.format(i+1, questions[i]['question']))
+ 
+ 
+for k,i in enumerate(selected_questions):
+    print('Question {}, {}'.format(k+1, questions[i]['question']))
     for j,o in enumerate(questions[i]['options']):
         print('{} - {}'.format(j+1, o))
     a = input('Please select the rigth answer : ')
     if a == str(questions[i]['answer']):
         score += 1
-        print('Good ! current score : {}/{}\n'.format(score, len(questions)))
+        print('Good ! current score : {}/{}\n'.format(score, 10))
     else:
-        print('False ! current score : {}/{}\n'.format(score, len(questions)))
+        print('False ! current score : {}/{}\n'.format(score, 10))
 
-print('Final score : {}/{}'.format(score, len(questions)))
-   '''
+print('Final score : {}/{}'.format(score, 10))
+
+file.close()
